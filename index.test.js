b2b6d5116ae361c3168ca5302c08a2a2eda68719
Achirawich <=>
2022-04-05 14:28:16 +0700

    success project

--- index.test.js
+++ index.test.js
@@ -1,5 +1,17 @@
-describe('Sample Test', () => {
-    it('should test that true === true', () =>{
-        expect(true).toBe(true)
-    })
+const request = require('supertest');
+const app = require('./index');
+
+describe('Server Test', () => {
+  it('should test that server start on port 3000', async () => {
+    await request(app);
+  })
+
+  it('should test api get', async () => {
+    const res = await request(app).get('/');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual({
+        status: true,
+        message: 'Hello World'
+    });
+  })
 })
